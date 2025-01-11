3a307f634ce8452f9d36f70759de0216b55cca16
sdkn104 <sdkn104@gmail.com>
2025-01-11 20:50:58 +0900

    Update myGmailWeb.js

--- static/myGmailWeb.js
+++ static/myGmailWeb.js
@@ -177,6 +177,8 @@ const myGmailWeb = {};
       } else {
         base64mailBody = message.result.payload.body.data;
       }
+      console.log(base64mailBody)
+      console.log(atob(base64mailBody))
       //result.Body = Buffer.from(base64mailBody, 'base64').toString(); //メール本文はBase64になってるので変
       const utf8Array = Uint8Array.from(
         Array.from(atob(base64mailBody)).map((s) => s.charCodeAt(0)),
